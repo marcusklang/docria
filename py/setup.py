f26ed4fc99558d3c6dd166cc57fc26b4377330cc
Marcus Klang <marcus.klang@cs.lth.se>
2018-12-12 09:49:37 +0100

    Bump in python version number

--- py/setup.py
+++ py/setup.py
@@ -14,7 +14,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='docria',
-    version='0.1.2',
+    version='0.1.3',
     description='Semi-structured Document Model',
     long_description=readme,
     long_description_content_type="text/x-rst",
